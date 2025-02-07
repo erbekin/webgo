ed3f00546a71d8fad35d660fceb82fa99e66ed2a
emre-erbekin <emreerbekin0@gmail.com>
2025-02-07 19:12:58 +0300

    Added basic auth and /echo & /secret /auth endpoints. These endpoints are subject to change.

--- internal/server/middlewares.go
+++ internal/server/middlewares.go
@@ -1,9 +1,19 @@
 package server
 
 import (
+	"context"
+	"crypto/sha256"
+	"crypto/subtle"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
+)
+
+const (
+	username = "admin"
+	password = "admin"
 )
 
 // Middleware: log requests
@@ -14,5 +24,51 @@ func loggingMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 		duration := time.Since(start)
 		log.Printf("INFO: İşlem süresi: %v", duration)
+		fmt.Println("---------------------------------------")
 	})
+}
+
+func basicAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get path
+		path := r.URL.Path
+		if strings.HasPrefix(path, "/auth/") || path == "/auth" {
+
+			isAuthorized := authUser(r)
+			if isAuthorized {
+				usr := BasicUser{
+					Username: username,
+					Password: password,
+					Auth:     true,
+				}
+				ctx := context.WithValue(r.Context(), "auth", usr)
+				next.ServeHTTP(w, r.WithContext(ctx))
+				return
+			}
+			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+			w.WriteHeader(401)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func authUser(r *http.Request) bool {
+	u, p, ok := r.BasicAuth()
+	if !ok {
+		return false
+	}
+
+	usernameHash := sha256.Sum256([]byte(u))
+	passwordHash := sha256.Sum256([]byte(p))
+	expectedUsername := sha256.Sum256([]byte(username))
+	expectedPassword := sha256.Sum256([]byte(password))
+	usernameMatch := subtle.ConstantTimeCompare(usernameHash[:], expectedUsername[:]) == 1
+	passwordMatch := subtle.ConstantTimeCompare(expectedPassword[:], passwordHash[:]) == 1
+	if usernameMatch && passwordMatch {
+		return true
+
+	} else {
+		return false
+	}
 }
--- internal/server/serve.go
+++ internal/server/serve.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -12,34 +14,89 @@ func Serve(addr string) error {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-			if _, err := fmt.Fprint(w, "<h1> Welcome </h1>"); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-			return
-		}
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		response := response{
-			Message: "Nothing to see here.",
-		}
-		if err := json.NewEncoder(w).Encode(response); err != nil {
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		if _, err := fmt.Fprint(w, "Hello, World!"); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-
+	})
+	mux.HandleFunc("GET /echo", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := io.ReadAll(r.Body)
+		encoded := base64.StdEncoding.EncodeToString(body)
+		cookiesJson, _ := json.Marshal(r.Cookies())
+		resp := echoResponse{
+			Method:  r.Method,
+			Proto:   r.Proto,
+			Headers: r.Header,
+			Body:    encoded,
+			Cookies: string(cookiesJson),
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		err := json.NewEncoder(w).Encode(resp)
+		if err != nil {
+			return
+		}
 	})
 
+	mux.HandleFunc("GET /auth", func(w http.ResponseWriter, r *http.Request) {
+		ref := r.URL.Query().Get("ref")
+		if ref == "" {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+		http.Redirect(w, r, ref, http.StatusFound)
+	})
+
+	mux.HandleFunc("GET /secret", func(w http.ResponseWriter, r *http.Request) {
+		isAuthorized := authUser(r)
+
+		if !isAuthorized {
+			http.Redirect(w, r, "/auth?ref=/secret", http.StatusFound)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		err := json.NewEncoder(w).Encode(struct {
+			Message string    `json:"message"`
+			User    BasicUser `json:"user-data"`
+		}{
+			Message: "Here is your secret",
+			User: BasicUser{
+				Username: username,
+				Password: password,
+			},
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+
+	})
 	// Use middleware
 	loggedMux := loggingMiddleware(mux)
+	authMux := basicAuthMiddleware(loggedMux)
 
 	// Start
 	log.Println("[Listening]" + addr)
 
-	return http.ListenAndServe(addr, loggedMux)
+	return http.ListenAndServe(addr, authMux)
 }
 
 type response struct {
 	Message string `json:"message"`
+}
+
+type echoResponse struct {
+	Proto   string      `json:"proto"`
+	Method  string      `json:"method"`
+	Headers http.Header `json:"headers"`
+	Body    string      `json:"body"`
+	Cookies string      `json:"cookies"`
+}
+
+type BasicUser struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Auth     bool   `json:"auth"`
 }
